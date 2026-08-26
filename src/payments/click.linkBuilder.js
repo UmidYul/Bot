d411e957d7dfb6a3600f9d300f630213c8895cd1
UmidYul <yumid253@gmail.com>
2026-08-26 14:32:03 +0500

    1231243

--- src/payments/click.linkBuilder.js
+++ src/payments/click.linkBuilder.js
@@ -1,20 +1,22 @@
 const config = require('../config');
 
 /**
- * Формирует ссылку на оплату Click для показа юзеру в боте.
- * Точный формат query-параметров подтверждается в личном кабинете Click при получении
- * реальных merchant-данных — ниже стандартная схема Checkout-ссылки Click Shop API,
- * помечена TODO-параметрами, которые могут потребовать корректировки.
+ * Формирует ссылку на оплату Click для показа юзеру в боте — стандартная схема
+ * Checkout-ссылки Click (см. docs.click.uz, "Платёжная ссылка"): my.click.uz/services/pay
+ * с service_id/merchant_id/merchant_user_id из личного кабинета. transaction_param — то же
+ * значение, которое Click потом вернёт как merchant_trans_id в вебхуках Prepare/Complete
+ * (см. src/payments/click.js).
  *
  * @param {{id: number, amount: number, merchant_trans_id: string}} payment
  * @param {{code: string}} user
  */
 function buildClickPayUrl(payment, user) {
   const params = new URLSearchParams({
-    service_id: config.click.serviceId, // TODO: сверить с личным кабинетом Click
-    merchant_id: config.click.merchantId, // TODO: сверить с личным кабинетом Click
+    service_id: config.click.serviceId,
+    merchant_id: config.click.merchantId,
+    merchant_user_id: config.click.merchantUserId,
     amount: String(payment.amount),
-    transaction_param: payment.merchant_trans_id, // TODO: поле, по которому Click вернёт merchant_trans_id в Prepare/Complete
+    transaction_param: payment.merchant_trans_id,
     return_url: config.webBaseUrl,
   });
 
